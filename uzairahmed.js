c759cbdfdb220627d07d333d25a7c494f679bd90
uzairahmed987 <156950220+uzairahmed987@users.noreply.github.com>
2024-03-01 07:46:44 -0800

    Add files via upload

---
+++ uzairahmed.js
@@ -0,0 +1,5 @@
+let number1 = prompt ("Enter tha name");
+let number2 = prompt ("father name"); 
+let number3 = prompt ("roll number");
+let number4 = prompt  ("age");
+alert ("thanks for providing information")
